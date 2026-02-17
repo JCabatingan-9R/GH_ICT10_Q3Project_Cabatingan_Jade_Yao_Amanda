9fe086693d1eea0aabb6929c20ff62cf594ba015
JCabatingan-9R <cabatingan.jc@obmontessori.edu.ph>
2026-02-17 08:10:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 from pyscript import display, document
 
 
+def check(e):
+    document.getElementById('output').innerHTML = '' # clear output
+
+    # get username and password values with their id
+    user = document.getElementById('user').value
+    pw = document.getElementById('pw').value
+
+    # turn variables into lists and get the number of characters using list() and len()
+    lu = list(user)
+    lp = list(pw)
+
+    username = len(lu)
+    password = len(lp)
+
+    # finding out if characters are all letters or all numbers for future conditional statements
+    pwlist = [pw.isdigit(), pw.isalpha()]
+
+    # conditional statements
+    if username >= 7: # username must contain 7 characters or more
+        if password >= 10: # password must contain 10 characters or more
+            if pw.isdigit() == True: # if all characters are digits
+                display(f'There must be at least one letter in your password.', target="output")
+            
+            elif  pw.isalpha() == True: # if all characters are letters
+                display(f'There must be at least one number in your password.', target="output")
+            
+            elif any(pwlist) == False: # if there are no True values in pwlist, meaning the password ins't all letters nor all numbers
+                display(f'Congratulations! You have successfully signed up!', target="output")
+        else:
+            display(f'Your password must contain 10 characters.', target="output")
+    else: 
+        display(f'Your username must contain at least 7 characters.', target="output")
+
+
 def sign(e):
     document.getElementById('output').innerHTML = '' #clear output
     #get values from html
@@ -29,3 +63,4 @@ def sign(e):
         elif sect == 'Topaz':
             display(f'Congratulations! You are part of the Yellow Tigers!', target='output')
             document.getElementById("image").innerHTML="<img src='yellow_tigers.jpg' height='300' width='350'>"
+
